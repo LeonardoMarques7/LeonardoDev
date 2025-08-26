9d9982c34f2470e58a8bd28184fbdd2e368fbeaf
LeonardoMarques7 <leonardoemcs155@gmail.com>
2025-08-26 15:18:55 -0300

    fix: erro no deploy

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
+  base: "/", // 👈 garante caminhos corretos no deploy
 })
